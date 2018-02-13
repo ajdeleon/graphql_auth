ce4ef01ada056ada42602d9579e28cf16335dcce
AJ de Leon <ajdeleon1064@gmail.com>
2018-02-13 01:31:18 -0500

    add login signup and logout mutations as well as ui for logging in and signing up

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,10 +1,17 @@
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLID } = graphql;
+const UserType = require('./user_type')
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    dummyField: { type: GraphQLID }
+    user: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        //graphql will return null if req.user not set by passport
+        return req.user
+      }
+    }
   }
 });
 
